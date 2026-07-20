862472c622ab4beeac937565344e0adc121eae36
OBS <88804146+bisects@users.noreply.github.com>
2026-07-19 18:35:17 -0600

    Update callback.js

--- api/spotify/callback.js
+++ api/spotify/callback.js
@@ -1,9 +1,41 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const code = req.query.code;
 
   if (!code) {
-    return res.status(400).send("Missing Spotify code");
+    return res.status(400).send("No Spotify code received");
   }
 
-  res.send("Spotify callback works!");
+  const tokenResponse = await fetch(
+    "https://accounts.spotify.com/api/token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " +
+          Buffer.from(
+            `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+          ).toString("base64"),
+      },
+      body: new URLSearchParams({
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri:
+          "https://pieces.lol/api/spotify/callback",
+      }),
+    }
+  );
+
+  const tokenData = await tokenResponse.json();
+
+  if (!tokenResponse.ok) {
+    return res.status(500).json(tokenData);
+  }
+
+  res.status(200).json({
+    message: "Spotify login successful!",
+    access_token: tokenData.access_token,
+    refresh_token: tokenData.refresh_token,
+    expires_in: tokenData.expires_in,
+  });
 }
